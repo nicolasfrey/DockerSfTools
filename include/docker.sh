22033561189e0f701e09a2726dc11bec1c8b467b
Nicolas F <nicolas.frey@lacooperativewelcoop.com>
2022-08-26 17:57:58 +0200

    Add artifactory config

--- include/docker.sh
+++ include/docker.sh
@@ -2,7 +2,7 @@
 
 # Test login on artifactory
 dockerIsConnected () {
-   docker login artifactory.groupe.pharmagest.com < /dev/null >& /dev/null
+   docker login "${APP__ARTIFACTORY_PATH}" < /dev/null >& /dev/null
 
    # shellcheck disable=SC2181
    if [ $? -eq 0 ]; then
@@ -15,8 +15,8 @@ dockerIsConnected () {
 # Login on artifactory
 dockerLogin () {
    if ! dockerIsConnected; then
-      echo "----> Docker login artifactory.groupe.pharmagest.com"
-      docker login artifactory.groupe.pharmagest.com 2> /dev/null || displayError "Failed to connect to artifactory server. Please try again !"
+      echo "----> Docker login ${APP__ARTIFACTORY_PATH}"
+      docker login "${APP__ARTIFACTORY_PATH}" 2> /dev/null || displayError "Failed to connect to artifactory server. Please try again !"
    fi
 }
 
@@ -63,7 +63,7 @@ dockerGetCommonPath () {
    dc_common_lib_path="${APP__COMMON_LIB_PATH/#\~/$HOME}/docker-compose.yml"
 
    if [ ! -f "${dc_common_lib_path}" ]; then
-      displayError "Common files not found. Clone git project and check your project configuration (.env or .env.local)."
+      displayError "Common files not found. Check your project configuration (.env or .env.local)."
       exit 1
    fi
 }
