ac1b5f0530c9a91fd80e0bd36351ece2cdb15b9e
Nicolas F <nicolas.frey@lacooperativewelcoop.com>
2022-09-30 16:18:14 +0200

    Check env file + new message

--- include/common.sh
+++ include/common.sh
@@ -17,8 +17,20 @@ displayMessage () {
    echo -e "\e[49m\e[39m"
 }
 
+displayWarning () {
+   TEXT=${1}
+   echo -e "\e[31m${TEXT}\e[39m"
+}
+
 # .env loading in the shell
 dotenv () {
+
+   if [[ ! -f .env ]]; then
+      displayError "Config file not found. Check, if you have .env file !"
+   fi
+
+   checkLineSeparators ".env"
+
    # Ajout d'un prefix APP__ sur les variables, sinon, on a un conflit avec docker
    if [[ -f .env ]]; then
       # shellcheck disable=SC2046
@@ -30,21 +42,31 @@ dotenv () {
       eval $(grep -v -e "^#" .env.local | sed "/^$/d" | xargs -I {} echo export \'APP__{}\')
    fi
 
-   if isDosFile "${APP__SYMFONY_APP_PATH}/.env" || isDosFile "${APP__SYMFONY_APP_PATH}/.env.staging" || isDosFile "${APP__SYMFONY_APP_PATH}/.env.prod" || isDosFile ".env"; then
-        echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!! Check line separator configuration !!!!!!!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        exit 1
-   fi
-
    # Lecture .env.prod si trouvé
    if [[ -f ${APP__SYMFONY_APP_PATH}/.env.prod ]]; then
+      checkLineSeparators "${APP__SYMFONY_APP_PATH}/.env.prod"
       # shellcheck disable=SC2046
       eval $(grep -P -e "^(DATABASE_URL|UPLOAD_DIR|SERVER_IP|SRC_PATH)" "${APP__SYMFONY_APP_PATH}/.env.prod" | xargs -I {} echo export \'APP_PROD__{}\')
+   else
+      displayWarning "Warning, \"${APP__SYMFONY_APP_PATH}/.env.prod\" not found !"
    fi
 
    # Lecture .env.staging si trouvé
    if [[ -f ${APP__SYMFONY_APP_PATH}/.env.staging ]]; then
+      checkLineSeparators "${APP__SYMFONY_APP_PATH}/.env.staging"
       # shellcheck disable=SC2046
       eval $(grep -P -e "^(DATABASE_URL|UPLOAD_DIR|SERVER_IP|SRC_PATH)" "${APP__SYMFONY_APP_PATH}/.env.staging" | xargs -I {} echo export \'APP_STAGING__{}\')
+   else
+         displayWarning "Warning, \"${APP__SYMFONY_APP_PATH}/.env.staging\" not found !"
+   fi
+}
+
+checkLineSeparators() {
+   if isDosFile "$1" ; then
+      echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      echo -e "! Check line separator configuration for $1"
+      echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      exit 1
    fi
 }
 
